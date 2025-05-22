c9bd4c6a2cb112d849319bfa48409f9a9437af90
vtrmq <vtrmq09@gmail.com>
2025-05-22 01:07:09 -0500

    cuatro WebSocket

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const WebSocket = require('ws');
 
 module.exports = (req, res) => {
-  // Log para depurar la solicitud
   console.log('Solicitud recibida:', req.url, req.headers);
 
-  // Verificar si es una solicitud WebSocket
-  if (req.headers.upgrade && req.headers.upgrade.toLowerCase() === 'websocket' && req.url === '/api') {
+  if (req.headers.upgrade && req.headers.upgrade.toLowerCase() === 'websocket') {
     console.log('Iniciando conexión WebSocket');
 
     const wss = new WebSocket.Server({ noServer: true });
@@ -28,17 +26,12 @@ module.exports = (req, res) => {
       });
     });
 
-    // Manejar la actualización WebSocket
     if (res.socket.server) {
       res.socket.server.on('upgrade', (request, socket, head) => {
-        if (request.url === '/api') {
-          console.log('Procesando upgrade para /api');
-          wss.handleUpgrade(request, socket, head, (ws) => {
-            wss.emit('connection', ws, request);
-          });
-        } else {
-          socket.destroy();
-        }
+        console.log('Procesando upgrade para:', request.url);
+        wss.handleUpgrade(request, socket, head, (ws) => {
+          wss.emit('connection', ws, request);
+        });
       });
     } else {
       console.error('No se encontró res.socket.server');
@@ -48,7 +41,6 @@ module.exports = (req, res) => {
 
     res.status(200).end();
   } else {
-    // Manejar solicitudes HTTP
     console.log('Solicitud HTTP normal');
     res.status(200).send('Use WebSocket to connect a client');
   }
